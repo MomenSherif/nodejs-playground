bd4475148affd858364875375c6b1732fd5d0d46
MomenSherif <momensherif.2019@gmail.com>
2020-03-14 23:10:48 +0200

    Printing in Color

--- notes-app/app.js
+++ notes-app/app.js
@@ -12,5 +12,18 @@
 // const getNotes = require('./notes');
 // console.log(getNotes());
 
-const validator = require('validator');
-console.log(validator.isEmail('momensherif.2019@gmail.com'));
+// const validator = require('validator');
+// console.log(validator.isEmail('momensherif.2019@gmail.com'));
+
+/**
+ * challenge: Use the chalk library in your project
+ *  1. Install chalk
+ *  2. Load chalk into app.js
+ *  3. Use it to print the string "Success!" to the console in green
+ *  Bonus: Use docs to mess around with other styles. Make text bold and inversed
+ */
+
+const chalk = require('chalk');
+
+console.log(chalk.green('Success!'));
+console.log(chalk.keyword('orange').bold('Success!'));
