3df9ebfa20ead65407f97f41a3aa8bae27f85300
MomenSherif <momensherif.2019@gmail.com>
2020-03-14 20:23:34 +0200

    Importing Your Own Files

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,13 @@
-const fs = require('fs');
-
-fs.writeFileSync('notes.txt', 'This file was created by Node.js!');
+// const sum = require('./utils');
+// console.log(sum(3, 6));
 
 /**
- * Challenge: Append a message to notes.txt
+ * Challenge: Define and use a function in a new file
+ *  1. Create new file called notes.js
+ *  2. Create getNotes function that returns "Your notes..."
+ *  3. Export getNotes function
+ *  4. From app.js, load in and call the function printing message to console
  */
 
-fs.appendFileSync('notes.txt', ` Mo'men Sherif was here...`);
+const getNotes = require('./notes');
+console.log(getNotes());
