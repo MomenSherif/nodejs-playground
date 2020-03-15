85d13e9b04250e0b6275d4ee73095d7fac6fc514
MomenSherif <momensherif.2019@gmail.com>
2020-03-15 12:43:25 +0200

    Arguments Parsing with Yargs Part |

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,20 +1,46 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes');
 
 const error = chalk.red;
 
-const command = process.argv[2];
+// Customize yargs version
+yargs.version('1.1.0');
 
-switch (command) {
-  case 'add':
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: function() {
     console.log(chalk.green('Adding note...'));
-    break;
-  case 'remove':
-    console.log(chalk.keyword('orange')('Removing note...'));
-    break;
-  default:
-    console.log(error('Invalid Command'));
-    break;
-}
+  }
+});
 
-console.log(process.argv);
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function() {
+    console.log(chalk.keyword('orange')('Removing note...'));
+  }
+});
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function() {
+    console.log(chalk.green('Reading a note...'));
+  }
+});
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'Listing your notes',
+  handler: function() {
+    console.log(chalk.keyword('orange')('Listing notes...'));
+  }
+});
+
+console.log(yargs.argv);
