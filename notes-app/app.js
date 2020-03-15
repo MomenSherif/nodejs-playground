adaa9eae304752ef209bb25af89f362aa41a1a70
MomenSherif <momensherif.2019@gmail.com>
2020-03-15 13:06:12 +0200

    Arguments Parsing with Yargs Part ||

--- notes-app/app.js
+++ notes-app/app.js
@@ -10,9 +10,22 @@ yargs.version('1.1.0');
 // Create add command
 yargs.command({
   command: 'add',
-  describe: 'Add a new note',
-  handler: function() {
-    console.log(chalk.green('Adding note...'));
+  describe: 'Add a note',
+  // command options --option="...."
+  builder: {
+    title: {
+      describe: 'Note Title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note Body Content',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function({ title, body }) {
+    console.log(chalk.keyword('orange')(`Title: ${title}\nBody ${body}`));
   }
 });
 
@@ -20,7 +33,7 @@ yargs.command({
 yargs.command({
   command: 'remove',
   describe: 'Remove a note',
-  handler: function() {
+  handler: function(args) {
     console.log(chalk.keyword('orange')('Removing note...'));
   }
 });
@@ -43,4 +56,5 @@ yargs.command({
   }
 });
 
-console.log(yargs.argv);
+// parsing arguments
+yargs.parse();
