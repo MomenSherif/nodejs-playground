ce0ee984f11e43c3882233969c0ea16e7ea8b3f2
MomenSherif <momensherif.2019@gmail.com>
2020-03-15 11:19:28 +0200

    Getting Input From Users

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,29 +1,20 @@
-// const sum = require('./utils');
-// console.log(sum(3, 6));
-
-/**
- * Challenge: Define and use a function in a new file
- *  1. Create new file called notes.js
- *  2. Create getNotes function that returns "Your notes..."
- *  3. Export getNotes function
- *  4. From app.js, load in and call the function printing message to console
- */
-
-// const getNotes = require('./notes');
-// console.log(getNotes());
-
-// const validator = require('validator');
-// console.log(validator.isEmail('momensherif.2019@gmail.com'));
-
-/**
- * challenge: Use the chalk library in your project
- *  1. Install chalk
- *  2. Load chalk into app.js
- *  3. Use it to print the string "Success!" to the console in green
- *  Bonus: Use docs to mess around with other styles. Make text bold and inversed
- */
-
 const chalk = require('chalk');
+const getNotes = require('./notes');
 
-console.log(chalk.green('Success!'));
-console.log(chalk.keyword('orange').bold('Success!'));
+const error = chalk.red;
+
+const command = process.argv[2];
+
+switch (command) {
+  case 'add':
+    console.log(chalk.green('Adding note...'));
+    break;
+  case 'remove':
+    console.log(chalk.keyword('orange')('Removing note...'));
+    break;
+  default:
+    console.log(error('Invalid Command'));
+    break;
+}
+
+console.log(process.argv);
