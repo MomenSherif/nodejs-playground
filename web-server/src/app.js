9c3e9b26404a3ce4860b894d3ad2f67437ec5182
MomenSherif <momensherif.2019@gmail.com>
2020-03-17 18:48:50 +0200

    Advanced Templating & 404 page

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,37 +1,41 @@
 const path = require('path');
-
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 const port = 3000;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get('/', (req, res) => {
   res.render('index', {
+    title: 'Weather',
     name: `Mo'men Sherif`
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about', {
-    title: 'About Page',
-    name: `Mo'men Sherif`.toUpperCase()
+    title: 'About',
+    name: `Mo'men Sherif`
   });
 });
 
 app.get('/help', (req, res) => {
   res.render('help', {
-    title: 'Help Page',
+    title: 'Help',
+    name: `Mo'men Sherif`,
     message: 'Just close my application and you will be fine'
   });
 });
@@ -40,6 +44,22 @@ app.get('/weather', (req, res) => {
   res.send({
     location: 'Cairo',
     forecast: 18
+  });
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: 404,
+    name: `Mo'men Sherif`,
+    errorMessage: 'Help article not found'
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: 404,
+    name: `Mo'men Sherif`,
+    errorMessage: 'Page not found'
   });
 });
 
