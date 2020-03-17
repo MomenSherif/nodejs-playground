77cfa2956c5cc88e7ae7386b9817ec9742136e94
MomenSherif <momensherif.2019@gmail.com>
2020-03-17 14:12:38 +0200

    Dynamic Pages with Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,7 +6,28 @@ const app = express();
 const port = 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    name: `Mo'men Sherif`
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Page',
+    name: `Mo'men Sherif`.toUpperCase()
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help Page',
+    message: 'Just close my application and you will be fine'
+  });
+});
 
 app.get('/weather', (req, res) => {
   res.send({
