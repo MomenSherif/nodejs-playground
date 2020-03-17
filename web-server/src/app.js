d3cf9fd0d5dc270e1d7859dee69d51b213ff09c1
MomenSherif <momensherif.2019@gmail.com>
2020-03-17 11:42:27 +0200

    Serving up HTML & JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,25 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello Express.js');
+  res.send('<h1>Hello Express.js</h1>');
 });
 
 app.get('/help', (req, res) => {
-  res.send('Help Page');
+  res.send('<h3>Help Page</h3>');
 });
 
 app.get('/about', (req, res) => {
-  res.send('About page');
+  res.send({
+    name: `Mo'men`,
+    age: 23
+  });
 });
 
 app.get('/weather', (req, res) => {
-  res.send('Weather Page');
+  res.send({
+    location: 'Cairo',
+    forecast: 18
+  });
 });
 
 app.listen(port, () => {
