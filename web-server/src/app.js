d2f05abb19c3680bf3a749b114fe11ac886b01b6
MomenSherif <momensherif.2019@gmail.com>
2020-03-17 12:29:09 +0200

    Serving up Static Assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,12 @@
+const path = require('path');
+
 const express = require('express');
+
 const app = express();
 const port = 3000;
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello Express.js</h1>');
-});
-
-app.get('/help', (req, res) => {
-  res.send('<h3>Help Page</h3>');
-});
-
-app.get('/about', (req, res) => {
-  res.send({
-    name: `Mo'men`,
-    age: 23
-  });
-});
+app.use(express.static(publicDirectoryPath));
 
 app.get('/weather', (req, res) => {
   res.send({
