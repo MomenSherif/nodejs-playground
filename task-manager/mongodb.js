9ea9b81e513005566c460a5797509d1355c0aba2
MomenSherif <momensherif.2019@gmail.com>
2020-03-19 14:37:28 +0200

    Inserting Documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -9,4 +9,56 @@ MongoClient.connect(connectionURL, (err, client) => {
   console.log('Connected successfully to server');
 
   const db = client.db(dbName);
+
+  // db.collection('users').insertOne(
+  //   {
+  //     name: `Mo'men`,
+  //     age: 23
+  //   },
+  //   (err, result) => {
+  //     assert.equal(err, null);
+
+  //     console.log(result.ops);
+  //   }
+  // );
+
+  // db.collection('users').insertMany(
+  //   [
+  //     {
+  //       name: 'Amr',
+  //       age: 23
+  //     },
+  //     {
+  //       name: 'Menna',
+  //       age: 22
+  //     }
+  //   ],
+  //   (err, result) => {
+  //     assert.equal(err, null);
+
+  //     console.log(result.ops);
+  //   }
+  // );
+
+  db.collection('tasks').insertMany(
+    [
+      {
+        description: 'Task 1',
+        completed: false
+      },
+      {
+        description: 'Task 2',
+        completed: true
+      },
+      {
+        description: 'Task 3',
+        completed: false
+      }
+    ],
+    (err, result) => {
+      assert.equal(err, null);
+
+      console.log(result.ops);
+    }
+  );
 });
