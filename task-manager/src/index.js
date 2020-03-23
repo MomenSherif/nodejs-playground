c1749c87f0998cd4d5785162b8dead96be800440
MomenSherif <momensherif.2019@gmail.com>
2020-03-23 17:56:41 +0200

    Integrating Async/Await

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -9,59 +9,67 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // Create User EndPoint
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
   const user = new User(req.body);
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch(e => {
-      res.status(400).send(e);
-    });
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
 // Read Users EndPoint
-app.get('/users', (req, res) => {
-  User.find({})
-    .then(users => res.send(users))
-    .catch(e => res.sendStatus(500));
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.sendStatus(500);
+  }
 });
 
 // Read User EndPoint
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
   const _id = req.params.id;
-  User.findById(_id)
-    .then(user => {
-      user ? res.send(user) : res.sendStatus(404);
-    })
-    .catch(e => res.sendStatus(404));
+  try {
+    const user = await User.findById(_id);
+    user ? res.send(user) : res.sendStatus(404);
+  } catch (e) {
+    res.sendStatus(500);
+  }
 });
 
-// Create Post EndPoint
-app.post('/tasks', (req, res) => {
+// Create Task EndPoint
+app.post('/tasks', async (req, res) => {
   const task = new Task(req.body);
-  task
-    .save()
-    .then(() => res.status(201).send(task))
-    .catch(e => res.status(400).send(e));
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
 // Read Tasks EndPoint
-app.get('/tasks', (req, res) => {
-  Task.find({})
-    .then(tasks => res.send(tasks))
-    .catch(e => res.sendStatus(500));
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (e) {
+    res.sendStatus(500);
+  }
 });
 
 // Read Task EndPoit
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
   const _id = req.params.id;
-  Task.findById(_id)
-    .then(task => {
-      task ? res.send(task) : res.sendStatus(404);
-    })
-    .catch(e => res.sendStatus(404));
+  try {
+    const task = await Task.findById(_id);
+    task ? res.send(task) : res.sendStatus(404);
+  } catch (e) {
+    res.sendStatus(500);
+  }
 });
 
 app.listen(port, () => {
