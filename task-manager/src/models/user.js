3eb9c47a934f26d58355f0cdd88cae2db46b5a0c
MomenSherif <momensherif.2019@gmail.com>
2020-03-26 19:24:04 +0200

    Hiding User Private Data

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -66,6 +66,15 @@ userSchema.methods.generateAuthToken = async function() {
   return token;
 };
 
+userSchema.methods.toJSON = function() {
+  const user = this;
+  const userObject = user.toObject();
+  delete userObject.password;
+  delete userObject.tokens;
+
+  return userObject;
+};
+
 // Add new method to the model
 userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email });
