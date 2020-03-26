e120941c67f740d19586720e6cf02950dc5df592
MomenSherif <momensherif.2019@gmail.com>
2020-03-26 20:17:55 +0200

    Authenticating User Endpoints

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -61,45 +61,31 @@ router.get('/users/me', auth, async (req, res) => {
   res.send(req.user);
 });
 
-// Read User EndPoint
-router.get('/users/:id', async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const user = await User.findById(_id);
-    user ? res.send(user) : res.sendStatus(404);
-  } catch (e) {
-    res.sendStatus(500);
-  }
-});
-
 // Update User EndPoint
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
   const updates = Object.keys(req.body);
-  const allowedUpdates = ['name', 'email', 'password', 'age'];
-  const isValidOperaion = updates.every(update =>
+  const allowedUpdates = ['name', 'email', 'age', 'password'];
+  const isValidOperation = updates.every(update =>
     allowedUpdates.includes(update)
   );
 
-  if (!isValidOperaion)
-    return res.status(400).send({ error: 'Invalid Updates!' });
+  if (!isValidOperation)
+    return res.status(400).send({ error: 'Invalid Updates' });
 
   try {
-    const user = await User.findById(req.params.id);
-    if (!user) return res.sendStatus(404);
-
-    updates.forEach(update => (user[update] = req.body[update]));
-    await user.save();
-    res.send(user);
+    updates.forEach(update => (req.user[update] = req.body[update]));
+    await req.user.save();
+    res.send({ user: req.user });
   } catch (e) {
     res.status(400).send(e);
   }
 });
 
 // Delete User EndPoint
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    user ? res.send(user) : res.sendStatus(404);
+    await req.user.remove();
+    res.send(req.user);
   } catch (e) {
     res.sendStatus(500);
   }
