ebcc513ca13a9ad3af278ad698a4de267751db80
MomenSherif <momensherif.2019@gmail.com>
2020-03-27 11:07:39 +0200

    Authenticating Task Endpoints

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -18,20 +18,20 @@ router.post('/tasks', auth, async (req, res) => {
 });
 
 // Read Tasks EndPoint
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
   try {
-    const tasks = await Task.find({});
-    res.send(tasks);
+    await req.user.populate('tasks').execPopulate(); // const tasks = await Task.find({owner: req.user._id})
+    res.send(req.user.tasks);
   } catch (e) {
     res.sendStatus(500);
   }
 });
 
 // Read Task EndPoit
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
   const _id = req.params.id;
   try {
-    const task = await Task.findById(_id);
+    const task = await Task.findOne({ _id, owner: req.user._id });
     task ? res.send(task) : res.sendStatus(404);
   } catch (e) {
     res.sendStatus(500);
@@ -39,7 +39,7 @@ router.get('/tasks/:id', async (req, res) => {
 });
 
 // Update Task EngPoint
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ['description', 'completed'];
   const isValidOpertaion = updates.every(update =>
@@ -50,7 +50,10 @@ router.patch('/tasks/:id', async (req, res) => {
     return res.status(400).send({ error: 'Invalid Updates!' });
 
   try {
-    const task = await Task.findById(req.params.id);
+    const task = await Task.findOne({
+      _id: req.params.id,
+      owner: req.user._id
+    });
     if (!task) return res.sendStatus(404);
 
     updates.forEach(update => (task[update] = req.body[update]));
@@ -62,9 +65,12 @@ router.patch('/tasks/:id', async (req, res) => {
 });
 
 // Delete Task EndPoint
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
   try {
-    const task = await Task.findByIdAndDelete(req.params.id);
+    const task = await Task.findOneAndDelete({
+      _id: req.params.id,
+      owner: req.user._id
+    });
     task ? res.send(task) : res.sendStatus(404);
   } catch (e) {
     res.sendStatus(500);
