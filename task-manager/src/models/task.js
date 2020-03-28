e5f0df71b28587392bfbe163ad6a34c78856278e
MomenSherif <momensherif.2019@gmail.com>
2020-03-28 10:17:37 +0200

    Filtering, Paginating and Sorting Data

--- task-manager/src/models/task.js
+++ task-manager/src/models/task.js
@@ -1,20 +1,25 @@
 const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    completed: {
+      type: Boolean,
+      default: false
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User'
+    }
+  },
+  { timestamps: true }
+);
 // Define Task Model
-const Task = mongoose.model('Task', {
-  description: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'User'
-  }
-});
+const Task = mongoose.model('Task', taskSchema);
 
 module.exports = Task;
--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -19,12 +19,36 @@ router.post('/tasks', auth, async (req, res) => {
 });
 
 // Read Tasks EndPoint
-
+/**
+ * GET /tasks?completed=false/true
+ * GET /tasks?limit=10&skip=0
+ * GET /tasks?sortyBy=createdAt:desc
+ */
 router.get('/tasks', auth, async (req, res) => {
-  try {
-    await req.user.populate('tasks').execPopulate(); // const tasks = await Task.find({owner: req.user._id})
-    res.send(req.user.tasks);
+  const match = {};
+  const sort = {};
 
+  if (req.query.completed)
+    match.completed = req.query.completed === 'true' ? true : false;
+
+  if (req.query.sortBy) {
+    const [field, order] = req.query.sortBy.split(':');
+    sort[field] = order === 'asc' ? 1 : -1;
+  }
+
+  try {
+    await req.user
+      .populate({
+        path: 'tasks',
+        match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+          sort
+        }
+      })
+      .execPopulate();
+    res.send(req.user.tasks);
   } catch (e) {
     res.sendStatus(500);
   }
@@ -44,7 +68,6 @@ router.get('/tasks/:id', auth, async (req, res) => {
 
 // Update Task EngPoint
 router.patch('/tasks/:id', auth, async (req, res) => {
-
   const updates = Object.keys(req.body);
   const allowedUpdates = ['description', 'completed'];
   const isValidOpertaion = updates.every(update =>
@@ -64,7 +87,6 @@ router.patch('/tasks/:id', auth, async (req, res) => {
     updates.forEach(update => (task[update] = req.body[update]));
     await task.save();
     res.send(task);
-
   } catch (e) {
     res.status(400).send(e);
   }
