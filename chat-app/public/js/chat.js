d21c192bec69cbe7d5824ca1977aaa48f9ed25ed
MomenSherif <momensherif.2019@gmail.com>
2020-04-08 17:53:16 +0200

    Event Acknowledgements

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,1 +1,29 @@
 const socket = io();
+const form = document.querySelector('form');
+socket.on('message', ({ message }) => {
+  console.log(message);
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const message = form.message.value.trim();
+  if (!message) return;
+
+  socket.emit('sendMessage', { message }, (error) => {
+    if (error) return console.log(error);
+    console.log('Message Delivered');
+  });
+  form.message.value = '';
+});
+
+document.getElementById('send-location').addEventListener('click', (e) => {
+  if (!navigator.geolocation)
+    return alert('Geolocation is not supproted by your browser');
+
+  navigator.geolocation.getCurrentPosition(({ coords }) => {
+    const { latitude, longitude } = coords;
+    socket.emit('sendLocation', { latitude, longitude }, () => {
+      console.log('Location Shared');
+    });
+  });
+});
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,8 +11,33 @@ const io = socketio(server);
 const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '../public')));
 
-io.on('connection', () => {
-  console.log('New WebSocket Connection');
+let count = 1;
+
+io.on('connection', (socket) => {
+  socket.emit('message', { message: 'Welcome! ' });
+  socket.broadcast.emit('message', { message: 'New user has joined!' });
+
+  socket.on('sendMessage', ({ message }, callback) => {
+    const filter = new Filter();
+    if (filter.isProfane(message)) {
+      return callback('Profanity is not allowed');
+    }
+
+    io.emit('message', { message });
+    callback();
+  });
+
+  socket.on('sendLocation', ({ latitude, longitude }, callback) => {
+    io.emit('message', {
+      message: `https://google.com/maps?q=${latitude},${longitude}`,
+    });
+
+    callback();
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('message', { message: 'User has disconnected' });
+  });
 });
 
 server.listen(port, () => {
